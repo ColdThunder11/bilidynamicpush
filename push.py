65b4fdc03fd85a8d856902bd0191814451ed42a4
ColdThunder11 <lslyj27761@gmail.com>
2020-11-19 17:22:29 +0800

    1.1

--- push.py
+++ push.py
@@ -6,10 +6,15 @@ from hoshino import aiorequests
 import time
 import nonebot
 from hoshino.priv import *
+from PIL import Image
+import random
 
 messageLengthLimit=0
 push_uids = {}
 push_times = {}
+room_states = {}
+isOnChecking = False
+bilibiliCookie = ''
 
 sv = Service('bili-dynamic', help_='''
 B站动态推送插件
@@ -45,7 +50,12 @@ def getLimitedMessage(originMsg):
 def loadConfig():
     global push_uids
     global push_times
+    global room_states
     global messageLengthLimit
+    global bilibiliCookie
+    push_uids = {}
+    push_times = {}
+    room_states = {}
     config_path = path.join(path.dirname(__file__),'config.json')
     with open(config_path,'r',encoding='utf8')as fp:
         conf = json.load(fp)
@@ -54,7 +64,17 @@ def loadConfig():
         for uid in keys:
             push_uids[uid] = conf['uid_bind'][uid]
             push_times[uid] = int(time.time())
-        sv.logger.info('B站动态推送配置文件加载成功')
+        for uid in keys:
+            room_states[uid] = False
+    #生成16位LIVE_BUVID，好像随机生成没事
+    list2 = []
+    for number in range(16):
+        str2 = str(random.randint(0, 9))
+        list2.append(str2)
+    b = "".join(list2)
+    bilibiliCookie='LIVE_BUVID=AUTO{random_id};'.format(random_id=b)
+    sv.logger.info('B站动态推送配置文件加载成功')
+    
 
 def saveConfig():
     config_path = path.join(path.dirname(__file__),'config.json')
@@ -69,7 +89,19 @@ def saveConfig():
         fp.seek(0)
         json.dump(conf,fp,indent=4)
         
-
+async def check_uid_exsist(uid):
+    header = {
+        'Referer': 'https://space.bilibili.com/{user_uid}/'.format(user_uid=uid)
+    }
+    try:
+        resp = await aiorequests.get('http://api.bilibili.com/x/space/acc/info?mid={user_uid}'.format(user_uid=uid), headers=header, timeout=20)
+        res = await resp.json()
+        if res['code']==0:
+            return True
+        return False
+    except Exception as e:
+        sv.logger.info('B站用户检查发生错误 '+e)
+        return False
 
 @sv.on_prefix('订阅动态')
 async def subscribe_dynamic(bot, ev):
@@ -83,17 +115,33 @@ async def subscribe_dynamic(bot, ev):
         await bot.send(ev, "请按照格式发送", at_sender=True)
         return
     if not ' ' in text:#仅当前群组
+        if not await check_uid_exsist(text):
+            await bot.send(ev, '订阅失败：用户不存在')
+            return
         if not text in push_uids:
             push_uids[text] = [str(ev.group_id)]
+            room_states[text] = False
         else:
+            if str(ev.group_id) in push_uids[text]:
+                await bot.send(ev, '订阅失败：请勿重复订阅')
+                return
             push_uids[text].append(str(ev.group_id))
+            push_times[uid] = int(time.time())
     else:
         subUid=text.split(' ')[0]
         subGroup=text.split(' ')[1]
+        if not await check_uid_exsist(subUid):
+            await bot.send(ev, '订阅失败：用户不存在')
+            return
         if not subUid in push_uids:
             push_uids[subUid] = [subGroup]
+            room_states[subUid] = False
         else:
+            if subGroup in push_uids[subUid]:
+                await bot.send(ev, '订阅失败：请勿重复订阅')
+                return
             push_uids[subUid].append(subGroup)
+        push_times[subUid] = int(time.time())
     saveConfig()
     await bot.send(ev, '订阅成功')
 
@@ -148,13 +196,20 @@ async def disubscribe_dynamic(bot, ev):
     await bot.send(ev, '取消订阅成功')
 
 @sv.scheduled_job('cron', minute='*/2')
-async def get_bili_dynamic():
+async def check_bili_dynamic():
     global push_times
+    global room_states
+    global isOnChecking
+    if isOnChecking:
+        return
+    isOnChecking = True
     if push_uids=={}:
         loadConfig()
     uids = push_uids.keys()
     sv.logger.info('B站动态检查开始')
     for uid in uids:
+        #if uid != "171818544":
+        #    continue
         header = {
             'Referer': 'https://space.bilibili.com/{user_uid}/'.format(user_uid=uid)
         }
@@ -162,6 +217,7 @@ async def get_bili_dynamic():
             resp = await aiorequests.get('https://api.vc.bilibili.com/dynamic_svr/v1/dynamic_svr/space_history?host_uid={user_uid}'.format(user_uid=uid), headers=header, timeout=20)
             res = await resp.json()
             cards=res['data']['cards']
+            #cards=[res['data']['cards'][10]]
             for card in cards:
                 sendCQCode=[]
                 uname=card['desc']['user_profile']['info']['uname']
@@ -176,16 +232,49 @@ async def get_bili_dynamic():
                     sendCQCode.append('发表了动态：\n')
                     content=card['card']
                     contentJo = json.loads(content)
-                    picturesCount=contentJo['item']['pictures_count']
-                    trueContent =contentJo['item']['description']
+                    picturesCount= contentJo['item']['pictures_count']
+                    trueContent = contentJo['item']['description']
                     trueContent = getLimitedMessage(trueContent)
                     sendCQCode.append(trueContent)
-                    if picturesCount>0 and picturesCount<4:
-                        pictureSrcs=[]
-                        for pic in contentJo['item']['pictures']:
-                            pictureSrcs.append(pic['img_src'])
-                        for downPic in pictureSrcs:
-                            sendCQCode.append(getImageCqCode(downPic))
+                    pictures=contentJo['item']['pictures']
+                    if picturesCount>0:
+                        isBigPicture = False
+                        firstPictureSize = [pictures[0]['img_width'],pictures[0]['img_height']]
+                        if picturesCount>=9:
+                            isBigPicture = True
+                            for i in range(9):
+                                if pictures[i]['img_width']!=firstPictureSize[0] or pictures[i]['img_height']!=firstPictureSize[1]:
+                                    isBigPicture=False
+                            if isBigPicture:
+                                pictureSrcs=[]
+                                for i in range(9):
+                                    pictureSrcs.append(pictures[i]['img_src'])
+                                imgPath = await make_big_image(pictureSrcs,firstPictureSize,9)
+                                sendCQCode.append(getImageCqCode('file:///'+imgPath))
+                        if picturesCount>=6 and not isBigPicture:
+                            isBigPicture = True
+                            for i in range(6):
+                                if pictures[i]['img_width']!=firstPictureSize[0] or pictures[i]['img_height']!=firstPictureSize[1]:
+                                    isBigPicture=False
+                            if isBigPicture:
+                                pictureSrcs=[]
+                                for i in range(6):
+                                    pictureSrcs.append(pictures[i]['img_src'])
+                                imgPath = await make_big_image(pictureSrcs,firstPictureSize,6)
+                                pictureSrcs=[]
+                                sendCQCode.append(getImageCqCode('file:///'+imgPath))
+                                if picturesCount>6:
+                                    for i in range(7,picturesCount):
+                                        pictureSrcs.append(pictures[i]['img_src'])
+                                    for downPic in pictureSrcs:
+                                        sendCQCode.append(getImageCqCode(downPic))
+                        if not isBigPicture:
+                            if picturesCount>0 and picturesCount<4:
+                                pictureSrcs=[]
+                                for pic in pictures:
+                                    pictureSrcs.append(pic['img_src'])
+                                for downPic in pictureSrcs:
+                                    sendCQCode.append(getImageCqCode(downPic))
                     sendCQCode.append('\nhttps://t.bilibili.com/{dynamicId}'.format(dynamicId=dynamicId))
                 elif dynamicType==4:#纯文字动态
                     sendCQCode.append(uname)
@@ -295,12 +384,94 @@ async def get_bili_dynamic():
                     await broadcast(msg,sv_name='bili-dynamic')
                 else:
                     await broadcast(msg,push_uids[uid])
-                time.sleep(1)
+                time.sleep(0.5)
             push_times[uid]=int(time.time())
         except Exception as e:
             sv.logger.info('B站动态检查发生错误 '+e)
     sv.logger.info('B站动态检查结束')
+    #直播状态检查
+    sv.logger.info('B站直播状态检查开始')
+    for uid in uids:
+        try:
+            header = {
+                'Referer': 'https://space.bilibili.com/{user_uid}/'.format(user_uid=uid),
+                'Cookie' : bilibiliCookie
+            }
+            resp = await aiorequests.get('https://api.live.bilibili.com/room/v1/Room/getRoomInfoOld?mid={user_id}'.format(user_id=uid),headers=header, timeout=20)
+            res = await resp.json()
+            if res['data']['liveStatus']==1 and not room_states[uid]:
+                room_states[uid]=True
+                sendCQCode=[]
+                userResp = await aiorequests.get('https://api.bilibili.com/x/space/acc/info?mid={user_id}'.format(user_id=uid), timeout=20)
+                userRes = await userResp.json()
+                userName = userRes['data']['name']
+                sendCQCode.append(userName)
+                sendCQCode.append('开播了：\n')
+                sendCQCode.append(res['data']['title'])
+                sendCQCode.append('\n')
+                sendCQCode.append(getImageCqCode(res['data']['cover']))
+                sendCQCode.append('\n')
+                sendCQCode.append(res['data']['url'])
+                msg=''.join(sendCQCode)
+                if push_uids[uid][0]=='all':
+                    await broadcast(msg,sv_name='bili-dynamic')
+                else:
+                    await broadcast(msg,push_uids[uid])
+            elif room_states[uid] and res['data']['liveStatus']==0:
+                room_states[uid]=False
+                sendCQCode=[]
+                userResp = await aiorequests.get('https://api.bilibili.com/x/space/acc/info?mid={user_id}'.format(user_id=uid), timeout=20)
+                userRes = await userResp.json()
+                userName = userRes['data']['name']
+                sendCQCode.append(userName)
+                sendCQCode.append('下播了')
+                msg=''.join(sendCQCode)
+                if push_uids[uid][0]=='all':
+                    await broadcast(msg,sv_name='bili-dynamic')
+                else:
+                    await broadcast(msg,push_uids[uid])
+            time.sleep(0.5)
+        except Exception as e:
+            sv.logger.info('B站直播检查发生错误 '+e)
+    sv.logger.info('B站直播状态检查结束')
+    isOnChecking = False
         
+async def make_big_image(image_urls,size,imageNum):
+    dirPath = path.join(path.dirname(__file__), 'res', 'image')
+    for url in image_urls:#下载全部图片
+        imageResp = await aiorequests.get(url)
+        image = await imageResp.content
+        imgFilename = url.rsplit('/',1)[1]
+        imagePath = path.join(dirPath, imgFilename)
+        with open(path.abspath(imagePath),'wb' ) as f:
+                f.write(image)
+    if imageNum == 9:
+        newImg = Image.new('RGB', (size[0]*3, size[1]*3), 255)
+        currentImageCount=0
+        for y in range(3):
+            for x in range(3):
+                img = Image.open(path.join(dirPath, image_urls[currentImageCount].rsplit('/',1)[1]))
+                newImg.paste(img,(x*size[0],y*size[1]))
+                currentImageCount+=1
+        savePath = image_urls[0].rsplit('/',1)[1].split('.')[0]+'make.jpg'
+        newImg.save(savePath)
+        return savePath
+    if imageNum == 6:
+        newImg = Image.new('RGB', (size[0]*3, size[1]*2), 255)
+        currentImageCount=0
+        for y in range(2):
+            for x in range(3):
+                img = Image.open(path.join(dirPath, image_urls[currentImageCount].rsplit('/',1)[1]))
+                sv.logger.info(path.join(dirPath, image_urls[currentImageCount].rsplit('/',1)[1]))
+                sv.logger.info(currentImageCount)
+                newImg.paste(img,(x*size[0],y*size[1]))
+                currentImageCount+=1
+        savePath = path.join(dirPath,image_urls[0].rsplit('/',1)[1].split('.')[0]+'_make.jpg')
+        newImg.save(savePath)
+        return savePath
+    pass
+
+
 @sv.on_fullmatch(('重新载入B站动态推送配置','重新载入动态推送配置'))
 async def reload_config(bot, ev):
     if check_priv(ev, SUPERUSER):
